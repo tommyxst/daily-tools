240be53e9546d3ae59d87f0fe37a984402f2ab73
txi <tommyxst@gmail.com>
2022-02-05 22:22:06 +0800

    init project with vue and typescript

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import antd from 'ant-design-vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(antd).use(store).use(router).mount('#app')
---
+++ src/views/ToDoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+      <a-input-search
+        :value="text"
+        placeholder="在这输入"
+        size="large"
+        @search="onSubmit"
+      >
+        <template #enterButton>
+          <a-button>确定</a-button>
+        </template>
+      </a-input-search>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class ToDoList extends Vue {
+  private text = ''
+
+  public onSubmit(): void {
+    console.log(this.text)
+  }
+}
+</script>
+
+<style>
+
+</style>
